aa87bf0a016af2496eaec69bff0ac8b8cc954261
Zythee3 <zmatheusguilherme2@gmail.com>
2024-04-30 15:50:30 -0300

    base code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tela_inicial from "./components/Tela_inicial/tela_inicial"
+
+const App = () => {
+  return (
+    <>
+    
+    <Tela_inicial/>
+    </>
+  )
+}
+
+export default App
