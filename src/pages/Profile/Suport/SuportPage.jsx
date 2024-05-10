90374c015bc3ebbfbf85eb10533f5fdf0384672c
HuannAnd <huannvicente14@outlook.com>
2024-05-10 13:48:15 -0300

    config: Changing styles of SuportPage

--- src/pages/Profile/Suport/SuportPage.jsx
+++ src/pages/Profile/Suport/SuportPage.jsx
@@ -3,28 +3,40 @@ import styles from "./SuportPage.module.css"
 import SearchBar from "@/common/components/SearchBar"
 import Button from "@/common/components/Button/Button"
 
-
 export default function SuportPage() {
-    return (
-        <main>
-            <SearchBar/>
-            <div className={styles.wrapper}>
-                <Button variant="inverted">Termos e Serviços</Button>
-                <Button variant="inverted">Ajuda com o Site</Button>
-                <Button variant="inverted">Contatos</Button>
-            </div>
-            <div className={styles.container}>
-                <h3 className={styles.title}>Enviar uma solicitação</h3>
-                <p className={styles.text}>Não importa se é um probleminha técnico<br/>
-                    ou um surto, estamos aqui para ajudar!</p>
-                <form className={styles.form}>
-                    <label >1. Escolha um Assunto</label>
-                    <textarea className={styles.textarea}></textarea>
-                    <label>2. Descrição</label>
-                    <textarea className={`${styles.textarea} ${styles.doubleHeight}`} ></textarea>
-                </form>
-                <Button>Enviar</Button>
-            </div>
-        </main>
-    )
+  return (
+    <div className={`${styles.suport} container`}>
+      <SearchBar
+        placeholder="Digite alguma coisa..."
+        className={styles.searchBar}
+      />
+      <div className={styles.wrapper}>
+        <Button variant="inverted" className={styles.button}>
+          Termos e Serviços
+        </Button>
+        <Button variant="inverted" className={styles.button}>
+          Ajuda com o Site
+        </Button>
+        <Button variant="inverted" className={styles.button}>
+          Contatos
+        </Button>
+      </div>
+      <div className={styles.container}>
+        <h3 className={styles.title}>Enviar uma solicitação</h3>
+        <p className={styles.text}>
+          Não importa se é um probleminha técnico ou um surto, estamos aqui para
+          ajudar!
+        </p>
+        <form className={styles.form}>
+          <label className={styles.label}>1. Escolha um Assunto</label>
+          <textarea className={styles.textarea}></textarea>
+          <label className={styles.label}>2. Descrição</label>
+          <textarea
+            className={`${styles.textarea} ${styles.doubleHeight}`}
+          ></textarea>
+          <Button className={styles.send}>Enviar</Button>
+        </form>
+      </div>
+    </div>
+  )
 }
