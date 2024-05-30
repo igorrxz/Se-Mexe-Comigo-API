a08718d24c0da476e83897e3ffa1b53c72c2fd9f
Rafaelsn11 <faelsantana001@gmail.com>
2024-05-30 17:45:02 -0300

    feat: first commit from FriendsProfile

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import ProfilePage from "@/pages/Profile/ProfilePage"
 import HomePage from "@/pages/HomePage/HomePage"
+import FriendsProfile from "./pages/FriendsProfile/FriendsProfile"
 
 export default function App(){
   return (
@@ -11,6 +12,7 @@ export default function App(){
       <Routes>
         <Route Component={ProfilePage} path="/profile" />
         <Route Component={HomePage} path="/" />
+        <Route Component={FriendsProfile} path="/friendsprofile"/>
       </Routes>
     </BrowserRouter>
   )
