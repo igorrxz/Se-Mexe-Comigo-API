309b43e6ad217f4b22ac2b989c64b8d3e7319d31
Yuri Ernani <yuriernania@gmail.com>
2024-05-30 21:59:22 -0300

    Adicionando Chat

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import ProfilePage from "@/pages/Profile/ProfilePage"
 import HomePage from "@/pages/HomePage/HomePage"
 import LoginPage from "./pages/LoginPage/LoginPage"
 import { CalendarPage } from "./pages/Calendar/CalendarPage"
+import Chat from "@/pages/Chat/ChatPage"
 
 export default function App(){
   return (
     <BrowserRouter>
       <Routes>
+        <Route Component={Chat} path="/" />
         <Route Component={CalendarPage} path="/calendar" />
-        <Route Component={LoginPage} path="/" />
+        <Route Component={LoginPage} path="/login" />
         <Route Component={ProfilePage} path="/profile" />
         <Route Component={HomePage} path="/home" />
       </Routes>
